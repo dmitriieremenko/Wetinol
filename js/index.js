9f51e814d369becd1c01a02d53835459f8eda8d4
dmitrii.eremenko <dmitrii.eremenko@gen.tech>
2020-11-29 19:16:51 +0200

    asdasdasd

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
 	$('#pagepiling').pagepiling( { 		
 		onLeave: function(index, nextIndex, direction){
-if(index == 1){
+if(index == 1 && direction =='down'){
 	document.querySelector(".header") .classList.add("header__section2")
 	console.log("сработало")
 
 }
 
-if(index == 2){
+if(index == 2 && direction =='up'){
 	document.querySelector(".header") .classList.remove("header__section2")
 	console.log("сработало")
 }
+
 	}});
 
 });
