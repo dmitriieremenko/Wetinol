3940d643691d7ef3d89c4f6e56d7c1e646c5bf84
dmitrii.eremenko <dmitrii.eremenko@gen.tech>
2020-11-29 18:28:22 +0200

    q12312

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling();
+	$('#pagepiling').pagepiling( { 		
+		onLeave: function(index, nextIndex, direction){
+if(index == 1){
+	document.querySelector(".header") .classList.add("header__section2")
+	console.log("сработало")
+
+}
+
+if(index == 2){
+	document.querySelector(".header") .classList.remove("header__section2")
+	console.log("сработало")
+}
+	}});
+
 });
